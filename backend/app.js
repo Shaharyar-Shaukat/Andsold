2084dce5f0582cc6e399e45b05515b7b04705ce3
shaharyar <shaharyar.shaukat@tum.de>
2020-06-08 16:28:08 +0200

    Added DB error Handle and signup validator middleware

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 
 
@@ -31,6 +32,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(expressValidator());
 
 // Routes Middleware
 app.use('/api/authentication',authenticationRoutes);
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 
 const { signup } = require("../controllers/authentication.js");
+const { userSignupValidator } = require('../helpers/validator');
 
-router.post("/signup",signup);
+router.post("/signup",userSignupValidator,signup);
 
 module.exports = router;
