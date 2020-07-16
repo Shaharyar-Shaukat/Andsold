be9cf7754641a3b8bdc6a0af287ca0a2e472f1a6
jkaleve <jonathan.kaleve@ippen-digital.de>
2020-07-16 17:08:59 +0200

    added user authorization; minorly changed authentication

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const { userById } = require('../controllers/user');
 const { orderById, list, create, read, update, remove } = require('../controllers/order');
-const { verifyJwt } = require('../controllers/authentication')
+const { authenticate, authorize } = require('../controllers/authentication')
 
-router.get('/list/:userId', verifyJwt, list);
+router.get('/list/:userId', authenticate, list);
 
-router.post('/create/:userId', verifyJwt, create);
-router.get('/:orderId/:userId', verifyJwt, read);
-router.put('/:orderId/:userId', verifyJwt, update);
-router.delete('/:orderId/:userId', verifyJwt, remove);
+router.post('/create/:userId', authenticate, create);
+router.get('/:orderId/:userId', authenticate, authorize, read);
+router.put('/:orderId/:userId', authenticate, authorize, update);
+router.delete('/:orderId/:userId', authenticate, authorize, remove);
 
 router.param('userId', userById);
 router.param('orderId', orderById);
